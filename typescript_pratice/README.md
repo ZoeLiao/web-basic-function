168344a1d56d2c3ce03682ea61e9228133ef50c4
ZoeLiao <zoejoyuliao@gmail.com>
2019-04-14 16:50:17 +0800

    Add typescript_practice

---
+++ typescript_pratice/README.md
@@ -0,0 +1,9 @@
+# Typescript practice
+
+## Install TypeScript
+- install node.js and npm
+- `npm install -g typescript`
+- `tsc <filename>.ts` 
+
+## TypeScript in 5 minutes:
+- Document: [TypeScript in 5 minutes](https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html)
---
+++ typescript_pratice/typeScript_in_5_minutess/greeter.js
@@ -0,0 +1,16 @@
+// type annotations
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+//let user = { firstName: "Jane", lastName: "User" };
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
