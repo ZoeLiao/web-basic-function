ed8e53121996e56f0873c8dc3da9f935d1d6c1c2
ZoeLiao <zoejoyuliao@gmail.com>
2019-05-19 19:36:31 +0800

    Fix the the url order of server.js

--- next_js/server.js
+++ next_js/server.js
@@ -8,13 +8,13 @@ const handle = app.getRequestHandler();
 app.prepare()
     .then(() => {
         const server = express();
-        server.get('*', (req, res) => {
-            return handle(req, res);
-        });
         server.get('/p/:id', (req, res) => {
             const actualPage = '/post';
             const queryParams = { id: req.params.id };
             app.render(req, res, actualPage, queryParams);
+        });
+		server.get('*', (req, res) => {
+            return handle(req, res);
         });
         server.listen(3000, (err) => {
             if (err) throw err;
