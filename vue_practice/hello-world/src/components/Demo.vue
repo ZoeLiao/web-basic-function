1a78d5828d14c1b98a29afa7b855039e124c6777
ZoeLiao <zoejoyuliao@gmail.com>
2020-04-09 22:42:26 +0800

    Add basic vue

---
+++ vue_practice/hello-world/src/components/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+    <a :herf="link">click me</a>
+    <p v-if="number > 3">Show me if number more than 3</p>
+    <p v-else>Show me if number less than 3</p>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data: {
+    link: 'http://localost:8080', 
+    number: 3
+  }
+}
+</script>
+
